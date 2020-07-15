182ba7d7b78e9593e54ff0d767ce54d03c88a7ad
shurupin <shurupin@gmail.com>
2020-07-15 22:51:49 +0300

    h5

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -13,30 +13,25 @@ public class Eratosthenes {
 
     private void sift(){
 
-            for (int i=2; i<=sieve.length-1; i++){
-                    for (int j=2; j<=sieve.length; j++){
-
-                        if (i*j <sieve.length) sieve[i*j]=false;
-            }}
-            sieve [0]=false;
+            for (int p=2; p<=sieve.length-1; p++){
+                    if (sieve[p]==true) {
+                        for (int j = 2; j <= sieve.length; j++) {
+                            if (j * p < sieve.length) sieve[j * p] = false;
+                        }
+                    }
+            }
+            sieve[0]=false;
             sieve[1]=false;
             sieve[2]=true;
-
-
     }
 
     public boolean isSimple(int n){
         return sieve[n];
     }
 
-
-
     public static void main(String[] args) {
         Eratosthenes er= new Eratosthenes(5000);
-
-
-        System.out.println(er.isSimple(5));
-
+        System.out.println(er.isSimple(1021));
     }
 
 }
