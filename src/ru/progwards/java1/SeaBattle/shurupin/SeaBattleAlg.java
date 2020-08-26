67234107b5ed8b8f3cd36c7b738fe2d49d032b63
shurupin <shurupin@gmail.com>
2020-08-26 15:35:13 +0300

    1.1

--- src/ru/progwards/java1/SeaBattle/shurupin/SeaBattleAlg.java
+++ src/ru/progwards/java1/SeaBattle/shurupin/SeaBattleAlg.java
@@ -11,6 +11,7 @@ public class SeaBattleAlg {
 public int vicpoint=0;
     public SeaBattle seaBattle;
 
+    int stcnt = 0;
 
     List ship = new ArrayList();
     int[][] pole = new int[10][10];
@@ -39,220 +40,177 @@ public int vicpoint=0;
         }
     }
 /////////////KILL////////////////////
-    void kill(int x, int y) {
-        //   System.out.println("kill   "+x+":"+y +"    vinpoint="+vicpoint);
-        //   toScr(pole);
-        /// corners
-        if (y == 9 && x == 9) {
-            fire(x - 1, y);
-            fire(x, y - 1);
-        }
-        if (y == 0 && x == 0) {
-            fire(x + 1, y);
-            fire(x, y + 1);
-        }
+void kill(int x, int y) {
+    //   System.out.println("kill   "+x+":"+y +"    vinpoint="+vicpoint);
+    //   toScr(pole);
+    /// corners
+    if (y == 9 && x == 9) {
+        fire(x - 1, y);
+        fire(x, y - 1);
+    }
+    if (y == 0 && x == 0) {
+        fire(x + 1, y);
+        fire(x, y + 1);
+    }
 
-        if (y == 0 && x == 9) {
-            fire(x - 1, y);
-            fire(x, y + 1);
-        }
-        if (y==9 && x==0){ fire (x+1,y);
-            fire(x,y-1);
-        }
+    if (y == 0 && x == 9) {
+        fire(x - 1, y);
+        fire(x, y + 1);
+    }
+    if (y==9 && x==0){ fire (x+1,y);
+        fire(x,y-1);
+    }
 
-        /////sides///
-        if (y==0){ fire(x+1,y);
-            fire(x+1,y);
-        }
-        if (y==9){ fire(x+1,y);
-            fire(x+1,y);
-        }
-        if (x==0){ fire(x,y+1);
-            fire(x,y-1);
-        }
-        if (x==9){ fire(x,y+1);
-            fire(x,y-1);
-        }
+    /////sides///
+    if (y==0){ fire(x+1,y);
+        fire(x+1,y);
+    }
+    if (y==9){ fire(x+1,y);
+        fire(x+1,y);
+    }
+    if (x==0){ fire(x,y+1);
+        fire(x,y-1);
+    }
+    if (x==9){ fire(x,y+1);
+        fire(x,y-1);
+    }
 
-       switch (fire(x, y + 1)) {
-            case 1:
-                //    System.out.println("case 1   ----KILL ");
-                fire(x,y+2);
-            case 2:
-                //    System.out.println("case 1   ----KILL ");
+    switch (fire(x, y + 1)) {
+        case 1:
+            //    System.out.println("case 1   ----KILL ");
+            fire(x,y+2);
+        case 2:
+            //    System.out.println("case 1   ----KILL ");
 
 
-           case 8: //System.out.println("case 3    ----KILL");
-               fire(x+1, y);
-           case -1:// System.out.println("case 4   ----KILL");
-               next();
-        }
+        case 8: //System.out.println("case 3    ----KILL");
+            fire(x+1, y);
+        case -1:// System.out.println("case 4   ----KILL");
+            next();
+    }
 
-        ////////////  checking  ////
-        if (y+1<10 && pole[y+1][x]==1) {
-
-            // System.out.println("kil1");
-            kill(x, y + 1);
-        }
-
-        else if (y-1>0 && pole[y-1][x]==1) {
-
-            //System.out.println("kil2");
-            kill(x, y - 1);
-        }
-
-        else   if (x+1 <10 && pole[y][x+1]==1) {
-
+    ////////////  checking  ////
+    if (y+1<10 && pole[y+1][x]==1) {
+        stcnt++;
+        // System.out.println("kil1");
+        if (stcnt < 2) kill(x, y + 1);
+        else next();
+    } else if (y-1>0 && pole[y-1][x]==1) {
+        stcnt++;
+        if (stcnt < 2) kill(x, y - 1);
+        else next();
+    } else   if (x+1 <10 && pole[y][x+1]==1) {
+        stcnt++;
+        if (stcnt < 2)
             //System.out.println("kil3");
             kill(x + 1, y);
-        }
-
-        else   if (x-1 >0 && pole[y][x-1]==1) {
-
+        else next();
+    } else if (x - 1 > 0 && pole[y][x - 1] == 1) {
+        stcnt++;
+        if (stcnt < 2)
             //System.out.println("kil4");
             kill(x - 1, y);
-        }
-
+        else next();
     }
-/////////DZONE///////////////////
-    void dzone() {
 
-        //   System.out.println("************* start of deadline *******************");
-        //   toScr(pole);
+}
 
-        int[] xd = new int[ship.size()];
-        int[] yd = new int[ship.size()];
-
-        if (ship.size() == 1) {
-            int[] temp = (int[]) ship.get(0);
-            int ty = temp[1];
-            int tx = temp[0];
-            int decty = ty - 1;
-            int incty = ty + 1;
-            int dectx = tx - 1;
-            int inctx = tx + 1;
-
-            if (dectx<0) dectx=tx ; if (inctx>9) inctx=tx;
-            if (decty<0) decty=ty ; if (incty>9) incty=ty;
+    ///+++++++++//dz////DZONE////dz///***********************************////////////
+    void dzone(int x, int y) {
 
 
+        System.out.println("************* start of deadline ********** /ship size =" + ship.size() + " /*********");
+        toScr(pole);
+
+        mark(x, y);
 
 
-
-            pole [dectx][decty]=8;  pole [dectx][ty]=8;  pole [dectx][incty]=8;
-            pole [tx][decty]=8;     pole [tx][ty]=8;  pole [tx][incty]=8;
-            pole [inctx][decty]=8;  pole [inctx][ty]=8;  pole [inctx][incty]=8;
-
-
-            // toScr(pole);
-
-
-        }
-        else {
-            // System.out.println("else deadzone*");
-            int[] s0 = (int[]) ship.get(0);
-            int[] s1 = (int[]) ship.get(1);
-
-            //    System.out.println(Arrays.toString(s0)+"       "+Arrays.toString(s1));
-            //    System.out.println(s0[1]==s1[1]);
-
-
-            if (s0[0] == s1[0]) {
-                for (int i = 0; i < ship.size(); i++) {
-
-                    int[] temp = (int[]) ship.get(i);
-                    int ty = temp[1];
-                    int tx = temp[0];
-
-                    int dectx = tx - 1;
-                    int inctx = tx + 1;
-
-                    int decty = ty - 1;
-                int incty = ty + 1;
-
-
-                    if (dectx < 0) dectx = tx;
-                    if (inctx > 9) inctx = tx;
-                    if (decty < 0) decty = ty;
-                    if (incty > 9) incty = ty;
-                    //       System.out.println(dectx+":"+ty+"      "+tx+":"+ty+"     "+inctx+":"+ty);
-                    pole[dectx][ty] = 8;
-                    pole[tx][ty] = 8;
-                    pole[inctx][ty] = 8;
-
+        if (y + 1 < 10 && pole[y + 1][x] == 1) {
+            mark(x, y + 1);
+            if (y + 2 < 10 && pole[y + 2][x] == 1) {
+                mark(x, y + 2);
+                if (y + 3 < 10 && pole[y + 3][x] == 1) {
+                    mark(x, y + 3);
                 }
             }
-            if (s0[1] == s1[1]) {
-                for (int i = 0; i < ship.size(); i++) {
-
-                    int[] temp = (int[]) ship.get(i);
-                    int ty = temp[1];
-                    int tx = temp[0];
-
-                    int decty = ty - 1;
-                    int incty = ty + 1;
-                    int dectx = tx - 1;
-                    int inctx = tx + 1;
-
-                    if (dectx < 0) dectx = tx;
-                    if (inctx > 9) inctx = tx;
-                    if (decty < 0) decty = ty;
-                    if (incty > 9) incty = ty;
-                    //       System.out.println(tx+":"+decty+"      "+tx+":"+ty+"     "+tx+":"+incty);
-                    pole[tx][decty] = 8;
-                    pole[tx][ty] = 8;
-                    pole[tx][incty] = 8;
-
+        }
+        if (y - 1 > 0 && pole[y - 1][x] == 1) {
+            mark(x, y - 1);
+            if (y - 2 > 0 && pole[y - 2][x] == 1) {
+                mark(x, y - 2);
+                if (y - 3 > 0 && pole[y - 3][x] == 1) {
+                    mark(x, y - 3);
                 }
-
             }
-
-
-            //  toScr(pole);
-            //  System.out.println("************* end of deadline *******************");
         }
-      ship.clear();
+        if (x + 1 < 10 && pole[y][x + 1] == 1) {
+            mark(x + 1, y);
+            if (x + 2 < 10 && pole[y][x + 2] == 1) {
+                mark(x + 2, y);
+                if (x + 3 < 10 && pole[y][x + 3] == 1) {
+                    mark(x + 3, y);
+                }
+            }
+        }
+        if (x - 1 > 0 && pole[y][x - 1] == 1) {
+            mark(x - 1, y);
+            if (x - 2 > 0 && pole[y][x - 2] == 1) {
+                mark(x - 2, y);
+                if (x - 3 > 0 && pole[y][x - 3] == 1) {
+                    mark(x - 3, y);
+                }
+            }
+        }
+
+        toScr(pole);
+        System.out.println("************* end of deadline *******************");
+
+
     }
-////////////////////////////////////////////////
-/*    void lineF(List ll) {
-        System.out.println("line");
-        int[] st = (int[]) ll.get(0);
-        int[] lt = (int[]) ll.get(ll.size() - 1);
+
+    ////////////////////////////////////////////////
+    void mark(int x, int y) {
+        System.out.println("************* working MARK of deadline ********start****** x=" + x + "* y=" + y + "****");
+        int ty = x;
+        int tx = y;
+        int decty = ty - 1;
+        int incty = ty + 1;
+        int dectx = tx - 1;
+        int inctx = tx + 1;
+
+        if (dectx < 0) dectx = tx;
+        if (inctx > 9) inctx = tx;
+        if (decty < 0) decty = ty;
+        if (incty > 9) incty = ty;
+
+        pole[dectx][decty] = 8;
+        pole[dectx][ty] = 8;
+        pole[dectx][incty] = 8;
+        pole[tx][decty] = 8;
+        pole[tx][ty] = 8;
+        pole[tx][incty] = 8;
+        pole[inctx][decty] = 8;
+        pole[inctx][ty] = 8;
+        pole[inctx][incty] = 8;
 
 
-        for (int i = 0; i < ll.size(); i++) {
-            int [] temp= (int [])ll.get(i);
-            System.out.println(ll.size()+"-size   "+Arrays.toString(temp));
-        }
-
-
-        if (st[0] == lt[0]) {
-            if (st[1] > lt[1] && lt[1] - 1 >= 0) fire(st[0], lt[1] - 1);
-            else if (st[1] > lt[1]) fire(st[0], st[1] + 1);
-            if (st[1] < lt[1] && lt[1] + 1 <= 9) fire(st[0], lt[1] + 1);
-            else if (st[1] < lt[1]) fire(st[0], st[1] - 1);
-        }
-        if (st[1] == lt[1]) {
-            if (st[0] > lt[0] && lt[0] - 1 >= 0) fire(lt[0] - 1, lt[1]);
-            else if (st[0] > lt[0]) fire(st[0] + 1, st[1]);
-            if (st[0] < lt[0] && lt[0] + 1 <= 9) fire(lt[0] + 1, lt[1]);
-            else if (st[0] < lt[0]) fire(st[0] - 1, st[1]);
-        }
-
-    }*/
+        toScr(pole);
+        System.out.println("************* working MARK of deadline *********end**********");
+    }
 //*************///!!!!!!!!!!!///FIRE//////!!!!!!!!!!!!!!!///*********************///
     public Integer fire(int x, int y) {
         if (vicpoint==10) return -1;
         int res=0;
         if (y < 0 || x < 0 || y > 9 || x > 9) return -1;
         if (pole[y][x] != 0) {
-            //   System.out.println(x + "," + y + " /*** Hitting place ***  / shots:" + vicpoint + "/  pole=" + pole[y][x]);
+            System.out.println(x + "," + y + " /*** Hitting place ***  / deads:" + vicpoint + "/  pole=" + pole[y][x]);
+            toScr(pole);
             return -1;
         }
 
         SeaBattle.FireResult fireResult = seaBattle.fire(x, y);
         shots++;
-        //  System.out.println(x+"<x,y>"+y+"- shot#"+shots+" -"+fireResult);
+        System.out.println(x + "<x,y>" + y + "- shot#" + shots + " -" + fireResult);
         switch (fireResult) {
             case MISS:
                 pole[y][x] = 8;
@@ -262,9 +220,9 @@ public int vicpoint=0;
                 pole[y][x] = 2;
                 ship.add(new int[]{y, x});
                 vicpoint++;
-                //  System.out.println("call DZONE from DESTROYED");
+                System.out.println("call DZONE from DESTROYED");
 
-                dzone();
+                dzone(x, y);
 
                 return  2;
             case HIT:
@@ -295,14 +253,15 @@ public int vicpoint=0;
             for (int j = 0; j < 10; j++) {
                 if (pole[j][i] == 0) fire(i,j);
             }}
+
     }
 
     // main
-/*    public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Sea battle");
         SeaBattle seaBattle = new SeaBattle();
         new SeaBattleAlg().battleAlgorithm(seaBattle);
         System.out.println(seaBattle.getResult());
-    }*/
+    }
 }
 
