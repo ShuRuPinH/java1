4a72cde135731093c67cf399f52ff9fab2193691
shurupin <shurupin@gmail.com>
2020-08-30 23:59:44 +0300

    H13

--- src/ru/progwards/java1/lessons/sets/SetOperations.java
+++ src/ru/progwards/java1/lessons/sets/SetOperations.java
@@ -1,34 +1,49 @@
 package ru.progwards.java1.lessons.sets;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class SetOperations {
 
 
     public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
-        set1.addAll(set2);
-        return set1;
-    }
-
-    public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
-        set1.retainAll(set2);
-        return set1;
-    }
-
-    public static Set<Integer> difference(Set<Integer> set1, Set<Integer> set2) {
-        set1.removeAll(intersection(set1, set2));
-        return set1;
-    }
-
-    public static Set<Integer> symDifference(final Set<Integer> set1, final Set<Integer> set2) {
-        Set<Integer> inter = intersection(set1, set2);
-        Set<Integer> s1 = set1;
-        Set<Integer> s2 = set2;
+        Set<Integer> s1 = new HashSet<>(set1);
+        Set<Integer> s2 = new HashSet<>(set2);
         s1.addAll(s2);
-        s1.removeAll(inter);
-
         return s1;
     }
 
+    public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> s1 = new HashSet<>(set1);
+        Set<Integer> s2 = new HashSet<>(set2);
+
+        s1.retainAll(s2);
+        return s1;
+    }
+
+    public static Set<Integer> difference(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> s1 = new HashSet<>(set1);
+        Set<Integer> s2 = new HashSet<>(set2);
+        s1.removeAll(intersection(s1, s2));
+        return s1;
+    }
+
+    public static Set<Integer> symDifference(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> s1 = new HashSet<>(set1);
+        Set<Integer> s2 = new HashSet<>(set2);
+        final Set<Integer> inter = intersection(s1, s2);
+        s1.addAll(s2);
+        s1.removeAll(inter);
+        return s1;
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> t1 = new HashSet<Integer>(Arrays.asList(7, 1, 2, 10, 11, 0));
+        Set<Integer> t2 = new HashSet<Integer>(Arrays.asList(7, 1, 2, 10, 11, 0));
+
+
+        System.out.println(Arrays.toString(symDifference(t1, t2).toArray()));
+    }
 
 }
